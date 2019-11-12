e1cb6a439c2f214f5f1524ce9a61810bd73afb16
Prashant960 <ptiwari10051@gmail.com>
2019-11-12 21:30:44 +0530

    Adding Password Validation

---
+++ valid_password.py
@@ -0,0 +1,25 @@
+def validate(password):
+    pwd_rule = {'num': 0, 'upp': 0, 'low': 0, 'spe': 0}
+    if password == '' or password == ' ':
+        return False
+    else:
+        for x in password:
+            if chr(32) < x < chr(128):
+                if x.isalpha():
+                    if x.islower():
+                        pwd_rule['low'] += 1
+                    elif x.isupper():
+                        pwd_rule['upp'] += 1
+                elif x.isdigit():
+                    pwd_rule['num'] += 1
+                else:
+                    pwd_rule['spe'] += 1
+        else:
+            if not(0 in pwd_rule.values()):
+                return True
+        return False
+
+print('# Password rules : \n-atleast one upper,\n-one lower,\n-one number and\n-one special character except space')
+password = input('Enter the password : ')
+print(validate(password))
+
